84cf63fda0f2472c758d8ca2e736ed5924f791c2
Leonardo Grava de Mendonça <leonardo.grava@outlook.com>
2025-08-17 19:06:57 -0300

    fix database error

--- initdb/init.sql
+++ initdb/init.sql
@@ -1,8 +1,21 @@
+-- Cria o banco de dados 'blog' se ele não existir
+DO
+$$
+BEGIN
+  IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'blog') THEN
+    PERFORM dblink_exec('dbname=' || current_database(), 'CREATE DATABASE blog');
+  END IF;
+END
+$$;
+
+-- Conecta à database 'blog'
+\c blog
+
 -- Cria a tabela 'posts' se ela não existir
 CREATE TABLE IF NOT EXISTS posts (
-  id SERIAL PRIMARY KEY,                -- Identificador único para cada post
-  title VARCHAR(255) NOT NULL,          -- Título do post (obrigatório)
-  content TEXT NOT NULL,                -- Conteúdo do post (obrigatório)
-  author VARCHAR(255) NOT NULL,         -- Autor do post (obrigatório)
-  created_at TIMESTAMP DEFAULT NOW()    -- Data e hora de criação do post (valor padrão: agora)
+  id SERIAL PRIMARY KEY,                
+  title VARCHAR(255) NOT NULL,          
+  content TEXT NOT NULL,                
+  author VARCHAR(255) NOT NULL,         
+  created_at TIMESTAMP DEFAULT NOW()    
 );
