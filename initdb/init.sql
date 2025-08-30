70949d4e1903942d083d4cad8d62b99f091c2b9d
Leonardo Grava de Mendonça <leonardo.grava@outlook.com>
2025-08-30 02:15:11 -0300

    corrigindo database initsql

--- initdb/init.sql
+++ initdb/init.sql
@@ -1,16 +1,3 @@
--- Cria o banco de dados 'blog' se ele não existir
-DO
-$$
-BEGIN
-  IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'blog') THEN
-    PERFORM dblink_exec('dbname=' || current_database(), 'CREATE DATABASE blog');
-  END IF;
-END
-$$;
-
--- Conecta à database 'blog'
-\c blog
-
 -- Cria a tabela 'posts' se ela não existir
 CREATE TABLE IF NOT EXISTS posts (
   id SERIAL PRIMARY KEY,                
