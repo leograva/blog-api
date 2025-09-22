c9ddf45b527a06e1be1e0324bf21d374e30c78c6
Leonardo Grava de Mendonça <leonardo.grava@outlook.com>
2025-09-22 02:13:56 -0300

    removendo arquivo desnecessário

--- initdb/run-init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-# Executa o script de inicialização do banco
-psql -h "$DB_HOST" -U "$DB_USER" -d "$DB_DATABASE" -f /initdb/init.sql
-
-echo "Script de inicialização executado com sucesso."
